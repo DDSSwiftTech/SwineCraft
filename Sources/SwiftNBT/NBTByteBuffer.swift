376fc2e07029a73bd0a9c17e11f4aaf37b14aac2
David Schwartz <dschwartz783@gmail.com>
2025-10-14 15:10:08 -0400

    add init for ByteBuffer for NBTEncodable

--- Sources/SwiftNBT/NBTByteBuffer.swift
+++ Sources/SwiftNBT/NBTByteBuffer.swift
@@ -1,6 +1,12 @@
 import NIOCore
 
 extension ByteBuffer {
+    init(_ node: any NBTEncodable) {
+        self.init()
+
+        node.encodeBody(&self)
+    }
+    
     mutating func writeNBT(_ node: any NBTEncodable) {
         node.encodeFull(&self)
     }
