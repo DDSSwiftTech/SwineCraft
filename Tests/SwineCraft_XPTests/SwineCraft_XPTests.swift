83984de3296ff18918458cdc519ba2d494ba6329
David Schwartz <dschwartz783@gmail.com>
2025-10-18 12:23:14 -0400

    remove example test

--- Tests/SwineCraft_XPTests/SwineCraft_XPTests.swift
+++ Tests/SwineCraft_XPTests/SwineCraft_XPTests.swift
@@ -4,10 +4,6 @@ import Testing
 @testable import SwineCraft_XP
 @testable import SwiftNBT
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Test func deflateInflateCompressDecompress() async throws {
     let compressor = DeflateCompressor()
     let decompressor = InflateDecompressor()
