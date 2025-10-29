9c71b497d1d7745df14f8001555503f584bfb835
David Schwartz <dschwartz783@gmail.com>
2025-10-28 21:10:00 -0400

    refactor a bit to do niftier things in MCPEPacket
    add encodeValue function, usable to encode Array values too

--- Sources/SwineCraft_XP/Packets/MCPEPacket.swift
+++ Sources/SwineCraft_XP/Packets/MCPEPacket.swift
@@ -14,47 +14,51 @@ extension MCPEPacket {
         buf.writeInteger(self.packetType.rawValue)
 
         for param in Mirror(reflecting: self).children {
-            if let value = param.value as? PlayStatusPacket.MCPEStatus {
-                buf.writeInteger(value.rawValue)
-            } else if let value = param.value as? CompressionMethod {
-                buf.writeInteger(value.rawValue)
-            } else if let value = param.value as? Bool {
-                buf.writeInteger(UInt8(value ? 1 : 0))
-            } else if let value = param.value as? UInt8 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? UInt16 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? UInt32 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? UInt64 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? UInt128 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Int8 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Int16 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Int32 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Int64 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Int128 {
-                buf.writeInteger(value)
-            } else if let value = param.value as? Float {
-                buf.writeInteger(value.bitPattern)
-            } else if let value = param.value as? UUID {
-                withUnsafeBytes(of: value.uuid) { ptr in
-                    let uuidInt = ptr.assumingMemoryBound(to: UInt128.self)
+            try self.encodeValue(param.value, buffer: &buf)
+        }
+    }
 
-                    buf.writeInteger(uuidInt.baseAddress!.pointee)
-                }
-            } else if let value = param.value as? [UInt8] {
-                buf.writeInteger(UInt16(value.count), endianness: .little)
-                buf.writeBytes(value)
-            } else if let value = param.value as? String {
-                try UnsignedVarInt(integerLiteral: UInt32(value.utf8.count)).encode(&buf)
-                buf.writeString(value)
+    func encodeValue(_ value: Any, buffer buf: inout ByteBuffer) throws {
+        if let value = value as? PlayStatusPacket.MCPEStatus {
+            buf.writeInteger(value.rawValue)
+        } else if let value = value as? CompressionMethod {
+            buf.writeInteger(value.rawValue)
+        } else if let value = value as? Bool {
+            buf.writeInteger(UInt8(value ? 1 : 0))
+        } else if let value = value as? UInt8 {
+            buf.writeInteger(value)
+        } else if let value = value as? UInt16 {
+            buf.writeInteger(value)
+        } else if let value = value as? UInt32 {
+            buf.writeInteger(value)
+        } else if let value = value as? UInt64 {
+            buf.writeInteger(value)
+        } else if let value = value as? UInt128 {
+            buf.writeInteger(value)
+        } else if let value = value as? Int8 {
+            buf.writeInteger(value)
+        } else if let value = value as? Int16 {
+            buf.writeInteger(value)
+        } else if let value = value as? Int32 {
+            buf.writeInteger(value)
+        } else if let value = value as? Int64 {
+            buf.writeInteger(value)
+        } else if let value = value as? Int128 {
+            buf.writeInteger(value)
+        } else if let value = value as? Float {
+            buf.writeInteger(value.bitPattern)
+        } else if let value = value as? UUID {
+            let _ = withUnsafeBytes(of: value.uuid) { ptr in
+                buf.writeBytes(ptr)
             }
+        } else if let value = value as? [any FixedWidthInteger] {
+            buf.writeInteger(UInt16(value.count), endianness: .little)
+            for item in value {
+                try self.encodeValue(item, buffer: &buf)
+            }
+        } else if let value = value as? String {
+            try UnsignedVarInt(integerLiteral: UInt32(value.utf8.count)).encode(&buf)
+            buf.writeString(value)
         }
     }
 }
